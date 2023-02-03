f74d3a183a8db1a766dea2fccfe75fc94b4ad112
Danielle Thoreson <danielle_thoreson@ymail.com>
2023-02-02 22:32:11 -0800

    more Algos updates

--- W1D4.js
+++ W1D4.js
@@ -1,10 +1,43 @@
 //Go with the Code Flow //
-var x = 5;
+
+// Code Flow-Functions //
+// var x = 5;
     
-function setX(newValue) {
-    x = newValue; 
+// function setX(newValue) {
+//     x = newValue; 
+// }
+    
+// console.log(x);
+// setX(15);
+// console.log(x);
+
+
+//The Return of return //
+// var x = 5;
+    
+// function addToX(amount) {
+//     return x + amount;
+//     console.log("hello there");
+// }
+    
+// console.log(x);
+// var result = addToX(-10);
+// console.log(result);
+// console.log(x);
+
+//Code Flow- Is the Array a Palindrome //
+function isPal(arr) {
+    for(var left=0; left<arr.length/2; left++) {
+        var right = arr.length-1-left;
+        if(arr[left] != arr[right]) {
+            return "Not a pal-indrome!";
+        }
+    }
+    return "Pal-indrome!";
 }
     
-console.log(x);
-setX(15);
-console.log(x);
+var result1 = isPal( [1, 1, 2, 2, 1] );
+console.log(result1);
+    
+var result2 = isPal( [3, 2, 1, 1, 2, 3] );
+console.log(result2);
